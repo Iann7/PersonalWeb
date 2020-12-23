0ce055ac3a3339c8105a17f1e0eb99b438007de4
Ignacio Iannello <ignacioiannello7@gmail.com>
2020-12-23 20:33:52 -0300

    ALMOST FINISHED

--- web.js
+++ web.js
@@ -1,7 +1,20 @@
 let i = 0;
-let arr = [{text:'Ignacio Iannello',id:"#mainTitle"},{text:'Soluciones Informaticas',id:"#secondTitle"}]
+let arr = [{text:'Ignacio Iannello',id:"#mainTitle"},
+{text:'Soluciones Informaticas',id:"#secondTitle"},
+{text:'Press enter to continue',id:"#enterText"}];
 let text = arr.shift();
-let timer = setInterval(writeText, 90);
+let timer = setInterval(writeText, 60);
+
+let timerMove;
+let hasFinished = false;
+let totalMoved =0;
+
+
+let divArr = [".about-me",".skills",".projects",".contact-me"];
+let onArr = 0;
+
+let timeScroll = 150;
+
 
 function writeText(){
   if(i < text.text.length){
@@ -14,6 +27,80 @@ function writeText(){
       text = arr.shift();
     } else {
       clearInterval(timer);
+      hasFinished = true;
     }
   }
+}
+
+
+
+document.addEventListener('keydown', function(event){
+
+  if(event.keyCode === 39 && hasFinished && onArr < divArr.length-1){
+    $(divArr[onArr]).animate({left:"-1500px"},timeScroll);
+     $(divArr[onArr]).delay(240).queue(function(next){
+       $(divArr[onArr]).css("visibility","hidden");
+       $(divArr[onArr]).css("right","auto");
+       $(divArr[onArr]).css("left","auto");
+
+        onArr++;
+       $(divArr[onArr]).css("right","-110%");
+       $(divArr[onArr]).css("visibility","visible");
+       $(divArr[onArr]).animate({right:"0%"},timeScroll);
+       next();
+
+  });
+}
+
+if(event.keyCode === 37 && hasFinished && onArr > 0){
+  $(divArr[onArr]).css("right","auto");
+  $(divArr[onArr]).css("left","auto");
+  $(divArr[onArr]).animate({right:"-1500px"},timeScroll);
+   $(divArr[onArr]).delay(240).queue(function(next){
+     $(divArr[onArr]).css("visibility","hidden");
+
+
+      onArr--;
+     $(divArr[onArr]).css("left","-110%");
+     $(divArr[onArr]).css("visibility","visible");
+     $(divArr[onArr]).animate({left:"0%"},timeScroll);
+
+     next();
+
+});
+}
+
+
+  if(event.keyCode === 13 && hasFinished ){
+
+     $(".overflow").animate({top:"-=1000px"},timeScroll);
+     $(".about-me").delay(240).queue(function(next){
+       $(".overflow").hide();
+       $(this).css("visibility","visible");
+       $(this).animate({top:"40%"},timeScroll);
+       next();
+     });
+  }
+
+/*if(event.keycode === 37 && onArr > 0 ){
+$(divArr[onArr]).animate({left:"-=1000px"},200);
+ $(divArr[onArr]).delay(240).queue(function(next){
+   $(divArr[onArr]).hide().css("left","0");
+    onArr--;
+   $(divArr[onArr]).css("visibility","visible");
+   $(divArr[onArr]).animate({right:"50%"},200);
+   next();
+ });
+}*/
+
+
+
+});
+
+
+
+
+function moveDiv(){
+  totalMoved+=20;
+  $(".overflow").animate({top:totalMoved + "px"});
 }
